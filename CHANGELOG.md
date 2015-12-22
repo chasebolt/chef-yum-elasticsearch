5b5796737ddc6c3f514582ccc6bef7c2da607143
Chase Bolt <cbolt@denirostaff.com>
2015-12-22 13:48:50 -0800

    updating CHANGELOG and bumping metadata

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # yum-elasticsearch Cookbook CHANGELOG
 This file is used to list changes made in each version of the yum-elasticsearch cookbook.
 
+## v0.2.0
+adding additional elasticsearch projects
+
 ## v0.1.0
 Initial release
--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Chase Bolt'
 maintainer_email 'chase.bolt@gmail.com'
 license 'Apache 2.0'
 description 'Installs and configures the Duo Security yum repository'
-version '0.1.0'
+version '0.2.0'
 
 source_url 'https://github.com/chasebolt/yum-elasticsearch'
 issues_url 'https://github.com/chasebolt/yum-elasticsearch/issues'

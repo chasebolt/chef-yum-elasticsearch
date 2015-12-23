909e7bfee405870a65becc6f50c8c92f4f92af24
Chase Bolt <cbolt@denirostaff.com>
2015-12-23 11:25:16 -0800

    easier management of yum resource

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,21 +3,25 @@ default['yum']['elasticsearch']['baseurl'] = 'http://packages.elastic.co/elastic
 default['yum']['elasticsearch']['gpgkey'] = 'http://packages.elastic.co/GPG-KEY-elasticsearch'
 default['yum']['elasticsearch']['gpgcheck'] = true
 default['yum']['elasticsearch']['enabled'] = true
+default['yum']['elasticsearch']['managed'] = true
 
 default['yum']['elasticsearch-beats']['description'] = 'Elastic Beats Repository'
 default['yum']['elasticsearch-beats']['baseurl'] = 'https://packages.elastic.co/beats/yum/el/$basearch'
 default['yum']['elasticsearch-beats']['gpgkey'] = 'http://packages.elastic.co/GPG-KEY-elasticsearch'
 default['yum']['elasticsearch-beats']['gpgcheck'] = true
 default['yum']['elasticsearch-beats']['enabled'] = true
+default['yum']['elasticsearch-beats']['managed'] = true
 
 default['yum']['elasticsearch-logstash']['description'] = 'Logstash Repository'
 default['yum']['elasticsearch-logstash']['baseurl'] = 'http://packages.elastic.co/logstash/2.1/centos'
 default['yum']['elasticsearch-logstash']['gpgkey'] = 'http://packages.elastic.co/GPG-KEY-elasticsearch'
 default['yum']['elasticsearch-logstash']['gpgcheck'] = true
 default['yum']['elasticsearch-logstash']['enabled'] = true
+default['yum']['elasticsearch-logstash']['managed'] = true
 
 default['yum']['elasticsearch-curator']['description'] = 'Curator Repository'
 default['yum']['elasticsearch-curator']['baseurl'] = 'http://packages.elastic.co/curator/3/centos/$releasever'
 default['yum']['elasticsearch-curator']['gpgkey'] = 'http://packages.elastic.co/GPG-KEY-elasticsearch'
 default['yum']['elasticsearch-curator']['gpgcheck'] = true
 default['yum']['elasticsearch-curator']['enabled'] = true
+default['yum']['elasticsearch-curator']['managed'] = true

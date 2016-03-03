2436acabd53a21da8c2d57ae4bbc467bd6336e7d
Chase Bolt <chase.bolt@gmail.com>
2016-03-03 11:30:55 -0800

    updating source_url and gems

--- metadata.rb
+++ metadata.rb
@@ -5,8 +5,8 @@ license 'Apache 2.0'
 description 'Installs and configures the Elasticsearch yum repository'
 version '0.2.5'
 
-source_url 'https://github.com/chasebolt/yum-elasticsearch'
-issues_url 'https://github.com/chasebolt/yum-elasticsearch/issues'
+source_url 'https://github.com/chasebolt/chef-yum-elasticsearch'
+issues_url 'https://github.com/chasebolt/chef-yum-elasticsearch/issues'
 
 depends 'yum', '~> 3.2'
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,6 @@
 #
 # Author:: Chase Bolt (<chase.bolt@gmail.com>)
-# Recipe:: yum::elasticsearch
+# Recipe:: yum-elasticsearch::default
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
